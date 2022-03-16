18ab848066621b054ae71130969e0dcc1f79fd9f
ndua1994 <duanishant71@gmail.com>
2022-03-16 23:46:06 +0530

    git test

--- admin_HCSrRaVt58Ezffv/application/config/database.php
+++ admin_HCSrRaVt58Ezffv/application/config/database.php
@@ -72,7 +72,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+/*
 
 $db['default'] = array(
 	'dsn'	=> '',
@@ -95,8 +95,8 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+*/
 
-/*
 
 $db['default'] = array(
 	'dsn'	=> '',
@@ -118,4 +118,4 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);*/
+);
