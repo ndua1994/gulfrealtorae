ac0d883e59d5177c3f693cd41750e648c34aad48
ndua1994 <duanishant71@gmail.com>
2022-03-16 23:39:35 +0530

    admin db configuration

--- admin_HCSrRaVt58Ezffv/application/config/database.php
+++ admin_HCSrRaVt58Ezffv/application/config/database.php
@@ -74,7 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 
-/*$db['default'] = array(
+$db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'gulfreal_gulfrea',
@@ -94,9 +94,9 @@ $query_builder = TRUE;
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);*/
+);
 
-
+/*
 
 $db['default'] = array(
 	'dsn'	=> '',
@@ -118,4 +118,4 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);
+);*/
